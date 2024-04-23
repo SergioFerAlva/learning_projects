5ad8e5b4ebf8d48423849a81254e0d57807a1816
SergioFerAlva <sergioferalva@gmail.com>
2024-04-23 15:42:36 +0200

    New project: arcade game

---
+++ alien_invasion/settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """a class to store all settings for Alien Invasion"""
+
+    def __init__(self):
+        """initialize the game's settings"""
+        #screen settings
+        self.screen_width = 1366
+        self.screen_height = 768
+        self.bg_color = (230, 230, 230)
+        
